4893976b6f452d395826fd2e5395db246b66c089
Heder Matheus <matheusmr640@gmail.com>
2021-05-04 02:27:41 -0300

    0227

---
+++ if/mini-prova2/Aula03-MP-Q2-Heder_Matheus.js
@@ -0,0 +1,22 @@
+/*
+ *
+ * Instituto Federal de Educação, Ciência e Tecnologia - IFPE
+ * Cursos: Informática para Internet | Sistemas para Internet
+ * Componentes Curriculares: Lógica de Programação e Estrutura de Dados | Introdução à Programação Imperativa
+ * Professor: Allan Lima - allan.lima@igarassu.ifpe.edu.br
+ *
+ * Nome completo: Heder Matheus Moura Paes Barreto
+ * Curso: TSI ou IPI
+ * Aula: Aula 03 - Variáveis, Tipos e Operadores - Mini-prova
+ * Questão: 2
+ *
+ */
+
+n1 = 2
+n2 = 5
+n3 = 6
+n4 = 5
+
+m = 4/((1/n1)+(1/n2)+(1/n3)+(1/n4))
+
+console.log('a média harmônica é', m)
---
+++ if/mini-prova2/Aula03-MP-Q3-Heder_Matheus.js
@@ -0,0 +1,27 @@
+/*
+ *
+ * Instituto Federal de Educação, Ciência e Tecnologia - IFPE
+ * Cursos: Informática para Internet | Sistemas para Internet
+ * Componentes Curriculares: Lógica de Programação e Estrutura de Dados | Introdução à Programação Imperativa
+ * Professor: Allan Lima - allan.lima@igarassu.ifpe.edu.br
+ *
+ * Nome completo: Heder Matheus Moura Paes Barreto
+ * Curso: TSI ou IPI
+ * Aula: Aula 03 - Variáveis, Tipos e Operadores - Mini-prova
+ * Questão: 3
+ *
+ */
+
+//var y := 1 -> não é necessario atribuir um valor a y, não se usa o : antes do =
+//const x := 10 -> não é necessario usar o const, não se usa o : antes do =
+//let a := -2 -> não é necessario usar o let, não se usa o : antes do =
+//let h := 3 -> não tem a h na equação, não se usa o : antes do =
+x = 10
+a = -2
+
+//y := x - a + 3 -> não se usa o : antes do =
+y = x - a + 3
+
+
+//console.log("valor","y") y -> Não precisa de aspas duplas no y
+console.log("valor :",y)
