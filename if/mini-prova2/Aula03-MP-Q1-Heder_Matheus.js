987755f32e78a7b9665fdba81cf4ff49ee2479c6
Heder Matheus <matheusmr640@gmail.com>
2021-05-04 01:41:07 -0300

    Create Aula03-MP-Q1-Heder_Matheus.js

---
+++ if/mini-prova2/Aula03-MP-Q1-Heder_Matheus.js
@@ -0,0 +1,20 @@
+/*
+ *
+ * Instituto Federal de Educação, Ciência e Tecnologia - IFPE
+ * Cursos: Informática para Internet | Sistemas para Internet
+ * Componentes Curriculares: Lógica de Programação e Estrutura de Dados | Introdução à Programação Imperativa
+ * Professor: Allan Lima - allan.lima@igarassu.ifpe.edu.br
+ *
+ * Nome completo: Heder Matheus Moura Paes Barreto
+ * Curso: TSI ou IPI
+ * Aula: Aula 03 - Variáveis, Tipos e Operadores - Mini-prova
+ * Questão: 1
+ *
+ */
+
+numcasos = 15
+taxa = 0.5
+y = 1+taxa
+numcasos = Math.pow(y,numcasos)
+
+console.log('o novo numero de casos é', numcasos)
