520c5f51ef0fc65497d3e76170577e24084f08c8
Heder Matheus <matheusmr640@gmail.com>
2021-05-04 00:49:41 -0300

    0049

---
+++ if/ex001.js
@@ -0,0 +1,6 @@
+
+l1 = 10
+
+perimetro = 3*l1
+
+console.log('o valor do perimetro é :', perimetro)
---
+++ if/ex002.js
@@ -0,0 +1,11 @@
+
+n1 = 10
+n2 = 7
+n3 = 4
+n4 = 6
+n5 = 1
+n6 = 15
+
+m = (n1+n2+n3+n4+n5+n6)/6
+
+console.log('a media é',m)
