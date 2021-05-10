1d2ea3cf51c78c33b222810202e29adbe6334e50
Heder Matheus <matheusmr640@gmail.com>
2021-05-10 01:30:20 -0300

    Create Aula04-MP-Q3-Heder_Matheus.js

---
+++ if/mini-prova3/Aula04-MP-Q3-Heder_Matheus.js
@@ -0,0 +1,33 @@
+/*
+ *
+ * Instituto Federal de Educação, Ciência e Tecnologia - IFPE
+ * Cursos: Informática para Internet | Sistemas para Internet
+ * Componentes Curriculares: Lógica de Programação e Estrutura de Dados | Introdução à Programação Imperativa
+ * Professor: Allan Lima - allan.lima@igarassu.ifpe.edu.br
+ *
+ * Nome completo: Heder Matheus Moura Paes Barreto
+ * Curso: TSI ou IPI
+ * Aula: Aula 04 - Execução Condicional - Mini-prova
+ * Questão: 3
+ *
+ */
+
+var n = 3
+
+if(n == 0){
+    console.log("o numero é neutro")
+}else if (n >= 0){
+    console.log("o numero é positivo")
+        if (n % 2 > 0){
+            console.log("o numero é impar")
+            } 
+        else {
+            console.log("o numero é par")
+        }
+    } 
+else if(n % 2 != 0){
+    console.log("o numero é negativo e impar")
+}
+else{
+    console.log("o numero é negativo e par")
+}
