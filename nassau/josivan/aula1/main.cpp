c5d9085c5e0e008d4049dae14a3dbce1940924e3
Heder Matheus <matheusmr640@gmail.com>
2021-09-20 20:04:42 -0300

    c++
    Aula 2

---
+++ nassau/josivan/aula1/main.cpp
@@ -0,0 +1,12 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    cout<<"Hello world\n";
+    system("PAUSE");
+    return 0;
+}
