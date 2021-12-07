48dbbd1d438ecb97d3452baeba39650155b9a589
Heder Matheus <matheusmr640@gmail.com>
2021-12-07 04:05:01 -0300

    web - av1

---
+++ if/2021.2/web1/ex6/cadastro.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+    <link rel="stylesheet" href="style/global.css">
+</head>
+<body>
+    <div class="voltar">
+        <a href="index.html" style="text-decoration:none">Voltar</a>
+    </div>
+    <form class="menu">
+        <div>
+            <br>
+            Login : <input type="text" name="login">
+            Senha : <input type="text" name="time"> 
+            <!--E-mail : <input type="text" name="email">
+            Nome : <input type="text" name="nome">
+            Time do coração : <input type="text" name="time"> -->
+        </div>
+        <div>
+            <br>
+            <input type="submit" value="Adicionar" class="submit">
+        </div>
+    </form>
+</body>
+</html>
