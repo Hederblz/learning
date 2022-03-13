14c58592122ed07f4d93156ad9cdfd1de9bdc6bb
Heder Matheus <matheusmr640@gmail.com>
2022-03-13 02:39:47 -0300

    cod3r

---
+++ cod3r/java/classe/Trauma.java
@@ -0,0 +1,12 @@
+package classe;
+
+public class Trauma {
+
+    int a = 3;
+
+    public static void main(String[] args) {
+
+        Trauma test = new Trauma();
+        System.out.println(test.a);
+    }
+}
